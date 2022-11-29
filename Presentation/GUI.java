13d04ba476c83c58fa5cb6ea2e2285f9c6fc58c3
Johan Sandager <johansandager@gmail.com>
2022-11-29 13:43:09 +0100

    Initial commit

---
+++ Presentation/GUI.java
@@ -0,0 +1,42 @@
+package Presentation;
+
+import javax.swing.*;
+import javax.swing.event.MenuEvent;
+
+import Domain.Media;
+import Domain.Movie;
+
+public class GUI {
+    
+    public static void main(String[] args) {
+        JFrame frame = makeMainFrame();
+
+        frame.setJMenuBar(makeJMenuBar());
+
+        //***TEST***
+        Media tis = new Movie(1, "Dj", 2002, null, 1.1, null);
+        frame.add(tis.display());
+        frame.validate();
+    } 
+
+    private static JFrame makeMainFrame() {
+        JFrame frame = new JFrame("NutFlix");
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        frame.setVisible(true);
+        return frame;
+    }
+
+    private static JMenuBar makeJMenuBar() {
+        JMenuBar menubar = new JMenuBar();
+        JMenuItem home = new JMenu("Hjem");
+        JMenuItem movies = new JMenu("Film");
+        JMenuItem series = new JMenu("Serier");
+
+        menubar.add(home);
+        menubar.add(movies);
+        menubar.add(series);
+
+        return menubar;
+    }
+
+}
