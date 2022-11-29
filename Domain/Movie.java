dc35d770a49f1809ae9ac5d94d53a03a549de383
Johan Sandager <johansandager@gmail.com>
2022-11-29 12:26:27 +0100

    changing visibility modifier of constructer

--- Domain/Movie.java
+++ Domain/Movie.java
@@ -3,7 +3,7 @@ import java.util.List;
 import java.awt.image.*;
 
 public class Movie extends Media {
-    Movie(int id, String title, int year, List<String> genre, double rating, BufferedImage image) {
+    public Movie(int id, String title, int year, List<String> genre, double rating, BufferedImage image) {
         super(id, title, year, genre, rating, image);
     }
 }
