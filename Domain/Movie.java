3172e2b30f42d10a0b9c4fbe76da75d1c060f779
Johan Sandager <johansandager@gmail.com>
2022-11-29 12:41:44 +0100

    adding display method

--- Domain/Movie.java
+++ Domain/Movie.java
@@ -1,9 +1,19 @@
 package Domain;
 import java.util.List;
+
+import javax.swing.JPanel;
+
 import java.awt.image.*;
 
-public class Movie extends Media {
+public class Movie extends Media implements Displayable {
     public Movie(int id, String title, int year, List<String> genre, double rating, BufferedImage image) {
         super(id, title, year, genre, rating, image);
+    }
+
+    @Override 
+    public JPanel display() {
+        JPanel panel = new JPanel();
+
+        return panel;
     }
 }
