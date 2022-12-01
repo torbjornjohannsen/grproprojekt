d9214c9dc4b38a614b1f70bdef9fb374f049a189
Johan Sandager <johansandager@gmail.com>
2022-12-01 12:36:30 +0100

    Adding demo of GUI

--- Presentation/GUI.java
+++ Presentation/GUI.java
@@ -1,21 +1,43 @@
 package Presentation;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.swing.*;
 import javax.swing.event.MenuEvent;
 
+import java.awt.GridLayout;
+
+import Data.TextDataAccess;
+import Domain.Displayable;
 import Domain.Media;
 import Domain.Movie;
+import Domain.State;
 
 public class GUI {
     
     public static void main(String[] args) {
         JFrame frame = makeMainFrame();
+        frame.setLayout(new GridLayout());
+
+        State state = new State(); 
+        TextDataAccess tda = new TextDataAccess();
+        state.init(tda.load("film"));
+        List<? extends Displayable> allDisplayables = state.getDisplayables();
+        System.out.println(allDisplayables.size());
 
         frame.setJMenuBar(makeJMenuBar());
 
-        //***TEST***
-        Media tis = new Movie(1, "Dj", "2002", null, 1.1, null);
-        frame.add(tis.display());
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(10, 10));
+        JScrollPane scrPane = new JScrollPane(panel);
+        
+        for(Displayable element : allDisplayables) {
+            
+            panel.add(element.display());
+            
+        }
+        frame.add(scrPane);
         frame.validate();
     } 
 
