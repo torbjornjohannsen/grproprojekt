0026cc4cfb58fbcd98570b22b2d353009280236d
tb <torbjornjohannsen@gmail.com>
2022-12-01 13:39:48 +0100

    user class

--- Domain/User.java
+++ Domain/User.java
@@ -7,17 +7,22 @@ public class User {
     protected String name;
     protected int age;
     protected String gender;
-    protected List<Media> watchHistory;
+    protected List<Integer> watchHistory;
+    protected List<Integer> favoriteList; 
 
     User(String name, int age, String gender) {
         this.name = name;
         this.age = age;
         this.gender = gender;
         this.watchHistory = new ArrayList<>();
+        this.favoriteList = new ArrayList<>();
     }
 
     public String getName() { return name; }
     public int getAge() { return age; }
     public String getGender() { return gender; }
+    public void AddFavorite(int favoriteID) { favoriteList.add(favoriteID); }
+    public void RemoveFavorite(int favoriteID) { favoriteList.remove((Object)favoriteID); } // need to cast to object otherwise it sees the int as an index
+    public void AddWHistory(int watchedID) { watchHistory.add(watchedID); }
 
 }
