01e78b51c8e3a559589ee385ed04158ed3d10085
Johan Sandager <johansandager@gmail.com>
2022-12-01 12:15:55 +0100

    Changed dipslay to ref image

--- Domain/Movie.java
+++ Domain/Movie.java
@@ -1,14 +1,12 @@
 package Domain;
 import java.util.List;
 
-import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
+import java.awt.GridLayout;
 import java.awt.image.*;
-import java.io.File;
-import java.io.IOException;
 
 public class Movie extends Media{
     public Movie(int id, String title, String year, List<String> genre, double rating, BufferedImage image) {
@@ -18,16 +16,13 @@ public class Movie extends Media{
     @Override 
     public JPanel display() {
         JPanel panel = new JPanel();
-        try {
-            BufferedImage img = ImageIO.read(new File("./MedieData/pictures/12 Angry Men.jpg"));
+        panel.setLayout(new GridLayout());
+            BufferedImage img = image;
             ImageIcon icon = new ImageIcon(img);
             JButton button = new JButton(icon);
             button.setBorderPainted(false);
             button.setFocusPainted(false);
             panel.add(button);
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
-        }
 
         return panel;
     }
